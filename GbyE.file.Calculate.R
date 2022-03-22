d3e97d2d9f9ba2acde8a9333694fe698c1143997
Xinrui Liu <97082499+liu-xinrui@users.noreply.github.com>
2022-03-22 16:46:49 +0800

    Add files via upload

--- GbyE.file.Calculate.R
+++ GbyE.file.Calculate.R
@@ -17,21 +17,36 @@ GbyE.file.Calculate <- function(GD,GM,file.output){
 	#rm(GD.d)
 	#rm(Matrix.r)
 	Taxa="taxa"
-	row.name=rbind(as.data.frame(c(as.character(Taxa),paste(GD[,1],"-1",sep=""),paste(GD[,1],"-2",sep=""))))
+	#row.name=rbind(as.data.frame(c(as.character(Taxa),paste(GD[,1],"-1",sep=""),paste(GD[,1],"-2",sep=""))))
+	row.name=rbind(as.data.frame(c(paste(GD[,1],"-1",sep=""),paste(GD[,1],"-2",sep=""))))
 	#row.name=rbind(as.matrix(Taxa),as.matrix(paste(GD[,1],"-1",sep="")),as.matrix(paste(GD[,1],"-2",sep="")))
-	col.name=t(as.matrix(GbyE.GM[,1]))
+	#col.name=t(as.matrix(GbyE.GM[,1]))
 	#GbyE.GD=rbind(col.name,apply(GbyE.GD,2,as.numeric))
-	GbyE.GD=rbind(col.name,GbyE.GD)
+	#GbyE.GD=data.frame(col.name,GbyE.GD)
+	#col.name=t(as.matrix(as.character(GbyE.GM[,1])))
+	#lm.GD=data.frame(matrix(NA,nrow(GbyE.GD)+1,ncol(GbyE.GD)))
+	#lm.GD[1,]=col.name
+	#lm.GD[-1,]=GbyE.GD
+	#GbyE.GD=as.martix(lm.GD)
 	GbyE.GD=cbind(row.name,GbyE.GD)
-	col.names=as.character(GbyE.GD[1,])
-	GbyE.GD=GbyE.GD[-1,]
-	colnames(GbyE.GD)=col.names
+	colnames(GbyE.GD)=c(paste(as.character(Taxa)),as.character(GbyE.GM[,1]))
+	#GbyE.GD=rbind(col.name,apply(GbyE.GD,2,as.numeric))
+	#GbyE.GD=cbind(row.name,GbyE.GD)
+	#GbyE.GD=data.frame(GbyE.GD)
+	#col.names=as.character(GbyE.GD[1,])
+	#GbyE.GD=GbyE.GD[-1,]
+	#colnames(GbyE.GD)=col.names
 	#rm(col.name)
 	#rm(row.name)
-	GbyE.GD[,-1]=apply(GbyE.GD[,-1],2,as.numeric)
+	#转化字符串
+	GbyE.GD[,1]=as.character(GbyE.GD[,1])
+	#转化数值型
+	#if(is.numeric(GbyE.GD[,-1])==FALSE){
+	#GbyE.GD[,-1]=apply(GbyE.GD[,-1],1,as.numeric)
+	#}
 	if(file.output==TRUE){
-						fwrite(GbyE.GD,paste(getwd(),"/","GbyE.GD.txt",sep=""),row.names=F,sep="\t",quote=FALSE,col.names=T)
-						fwrite(GbyE.GM,paste(getwd(),"/","GbyE.GM.txt",sep=""),row.names=F,sep="\t",quote=FALSE,col.names=T)
+						fwrite(GbyE.GD,paste(getwd(),"/","GbyE.GD.txt",sep=""),row.names=F,col.names=T,sep="\t",quote=FALSE)
+						fwrite(GbyE.GM,paste(getwd(),"/","GbyE.GM.txt",sep=""),row.names=F,col.names=T,sep="\t",quote=FALSE)
 						}else{
 								print("~~~~~~~~~The GbyE file is not output~~~~~~~~~~")
 								print("~~~~~~~~~If you want the file output~~~~~~~~~~")
